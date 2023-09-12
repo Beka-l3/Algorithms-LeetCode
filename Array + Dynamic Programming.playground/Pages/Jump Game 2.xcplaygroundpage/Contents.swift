b4e8d6e98bf9466ebea191a95377e8761aff2335
Beka-l3 <b.talgat@innopolis.university>
2023-09-12 20:43:58 +0300

    Jump game 2, main function

--- Array + Dynamic Programming.playground/Pages/Jump Game 2.xcplaygroundpage/Contents.swift
+++ Array + Dynamic Programming.playground/Pages/Jump Game 2.xcplaygroundpage/Contents.swift
@@ -1,6 +1,17 @@
 import Foundation
 
 func jump(_ nums: [Int]) -> Int {
+    var maxReachable = 0, last = 0, i = 0, jumps = 0
     
-    return .zero
+    while last < nums.count - 1 {
+        maxReachable = max(maxReachable, i + nums[i])
+        if i == last {
+            last = maxReachable
+            jumps += 1
+        }
+        
+        i += 1
+    }
+    
+    return jumps
 }
