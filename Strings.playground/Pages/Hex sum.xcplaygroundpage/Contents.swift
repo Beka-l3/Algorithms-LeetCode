e1835d4edaf018d3967bf1ca248bad7abcb4203c
Beka-l3 <b.talgat@innopolis.university>
2023-11-01 20:16:25 +0300

    Hex sum

---
+++ Strings.playground/Pages/Hex sum.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let cd = [
+    Character("0"): 0, Character("1"): 1,
+    Character("2"): 2, Character("3"): 3,
+    Character("4"): 4, Character("5"): 5,
+    Character("6"): 6, Character("7"): 7,
+    Character("8"): 8, Character("9"): 9,
+    Character("a"): 10, Character("b"): 11,
+    Character("c"): 12, Character("d"): 13,
+    Character("e"): 14, Character("f"): 15,
+]
+
+let dc = [
+    0: Character("0"), 1: Character("1"),
+    2: Character("2"), 3: Character("3"),
+    4: Character("4"), 5: Character("5"),
+    6: Character("6"), 7: Character("7"),
+    8: Character("8"), 9: Character("9"),
+    10: Character("a"), 11: Character("b"),
+    12: Character("c"), 13: Character("d"),
+    14: Character("e"), 15: Character("f"),
+]
