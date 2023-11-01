7a967a0b5f029d8f57dc28b7287a024b39365764
Beka-l3 <b.talgat@innopolis.university>
2023-11-01 20:16:55 +0300

    Hex sum

--- Strings.playground/Pages/Hex sum.xcplaygroundpage/Contents.swift
+++ Strings.playground/Pages/Hex sum.xcplaygroundpage/Contents.swift
@@ -21,3 +21,30 @@ let dc = [
     12: Character("c"), 13: Character("d"),
     14: Character("e"), 15: Character("f"),
 ]
+
+
+/// time O(n)       space O(1) -> O(n)
+/// Start from the end
+/// sum and count carry
+func sum(_ a: String, _ b: String) -> String {
+    let a = Array(a.isEmpty ? "0" : a), b = Array(b.isEmpty ? "0" : b)
+    
+    var i = a.count - 1, j = b.count - 1
+    var carry = false
+    var res = ""
+    
+    while i >= 0 || j >= 0 {
+        let h1 = i >= 0 ? cd[a[i]]! : 0
+        let h2 = j >= 0 ? cd[b[j]]! : 0
+        
+        let sum = h1 + h2 + (carry ? 1 : 0)
+        carry = sum >= 16
+        
+        res = "\(dc[sum % 16]!)" + res
+        
+        i -= 1
+        j -= 1
+    }
+    
+    return carry ? "1" + res : res
+}
