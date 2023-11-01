38fefd45732794d74065e559ea609ef04f6bb2bf
Beka-l3 <b.talgat@innopolis.university>
2023-11-01 20:15:40 +0300

    String multiplication by digit

--- Strings.playground/Pages/String multiplication by digit.xcplaygroundpage/Contents.swift
+++ Strings.playground/Pages/String multiplication by digit.xcplaygroundpage/Contents.swift
@@ -1,6 +1,9 @@
 import Foundation
 
-
+/// time O(n)       space O(1)
+/// start from the end of the string. inplace multiplication by digit
+/// here `carry` can be greater than `1`
+/// `edge case` after `while` check for `carry` again
 func multiplyByDigit(_ num: inout String, _ digit: String) {
     var i = num.count - 1, idx = num.index(num.startIndex, offsetBy: i), carry = 0
     let d = Int(digit)!
